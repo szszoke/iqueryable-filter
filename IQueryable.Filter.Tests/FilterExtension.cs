440b59862712f14e79f6a85c0074f1addd1c25f1
Szabolcs Szoke <szszoke.code@gmail.com>
2018-07-24 13:04:18 +0200

    Remove unused namespaces

--- IQueryable.Filter.Tests/FilterExtension.cs
+++ IQueryable.Filter.Tests/FilterExtension.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using IQueryable.Filter.Lib;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Logging.Console;
 using Xunit;
 
 namespace IQueryable.Filter.Tests
